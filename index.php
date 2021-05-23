c0afd2f11e3938b11f6216b67d654326f6c6d6a7
Your Name <you@example.com>
2021-05-23 21:58:07 +0200

    this is second commit for php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>test</title>
 </head>
 <body>
 
@@ -64,7 +64,7 @@ $query = "SELECT * FROM posts" ;
 
 
 
-
+<h1>this is test</h1>
 
 
 </body>
