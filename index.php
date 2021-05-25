f52341748937c071014910d8556222d1d79f6222
Your Name <you@example.com>
2021-05-25 09:17:51 +0200

    this is commit sen

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $query = "SELECT * FROM posts" ;
                  ?>
                  
                 <h2>
-                    <a href="post/<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                    <a href="text.php"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="author_posts.php?author=<?php echo $post_user ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_user ?></a>
